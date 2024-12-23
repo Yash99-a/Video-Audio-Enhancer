05d4c84d13b3f45c415a4b93d0dcc5b4f9ac04b6
Yash Shete <yashshete87@gmail.com>
2024-12-23 20:37:09 +0530

    app.py

--- app.py
+++ app.py
@@ -227,4 +227,4 @@ def download_video(video_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
